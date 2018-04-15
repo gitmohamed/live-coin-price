488dc57525a7d611a87bf2e32620d1f738aaa704
Mohamed Hassan <mohamedhdesign@gmail.com>
2018-04-15 00:27:23 -0700

    Adding 24h difference to output. #14 XO

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
   priceGetter(userCoin, (price, symbol, change) => {
     let diffSym = `${change.charAt(0) == "-" ? change : "+" + change}`;
     let discordMessage = ` **${symbol}** Value in USD:
-                             ${"```diff"}
-                             ${ diffSym }%
+                             ${"```autohotkey"}
+                             ^Green ${ diffSym }% ::
                             $${ price }${"```"}`;
     // console.log(price, symbol);
     e.message.channel.sendMessage(discordMessage);
