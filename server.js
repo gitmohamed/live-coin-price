53b26137256f84a410774be6e81ef7016a51ba19
Mohamed Hassan <mohamedhdesign@gmail.com>
2018-04-14 22:30:56 -0700

    Testing API output #6

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ client.Dispatcher.on("GATEWAY_READY", e => {
 
 // Callback function that returns price of coin in first argument
 let priceGetter = (coin, cb) => {
-  request.get(`https://api.coinmarketcap.com/v1/ticker/?start=0&limit=1000`, (err, resp, body) => {
+  request.get(`https://api.coinmarketcap.com/v1/ticker/?start=0&limit=10`, (err, resp, body) => {
     if (err) {
       console.log(err);
       return;
@@ -29,7 +29,7 @@ let priceGetter = (coin, cb) => {
     for (var i = 0; i < JSON.parse(body).length; i++) {
       if (coin === JSON.parse(body)[i].symbol) {
         console.log(JSON.parse(body).price_usd, JSON.parse(body).symbol);
-        // cb(JSON.parse(body).price_usd, JSON.parse(body).symbol);  
+        // cb(JSON.parse(body).price_usd, JSON.parse(body).symbol);
       }
     };
   });
