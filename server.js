41a7f9c91fb7cfc1b8deeb2cfcb0163785c19a0e
Mohamed Hassan <mohamedhdesign@gmail.com>
2018-04-15 03:20:39 -0700

    Fixed critical bot command error.

--- server.js
+++ server.js
@@ -41,77 +41,17 @@ let priceGetter = (coin, cb) => {
 // Event for when message is seen by the bot
 client.Dispatcher.on("MESSAGE_CREATE", e => {
   let userCoin = e.message.content.toUpperCase();
-  userCoin = userCoin.substr(1);
-  priceGetter(userCoin, (price, symbol, change) => {
-    let diffSym = `${change.charAt(0) == "-" ? "- " + change.substr(1) : "+ " + change}`;
-    let discordMessage = ` **${symbol}** Value in USD (With 24HR CHANGE :money_mouth:):
+  if (userCoin.charAt(0) === "$") {
+    userCoin = userCoin.substr(1);
+    priceGetter(userCoin, (price, symbol, change) => {
+      let diffSym = `${change.charAt(0) == "-" ? "- " + change.substr(1) : "+ " + change}`;
+      let discordMessage = ` **${symbol}** Value in USD (With 24HR CHANGE :money_mouth:):
 ${"```diff"}
 ${ diffSym }%
 $${ price }${"```"}`;
     // console.log(price, symbol);
-    e.message.channel.sendMessage(discordMessage);
-    return;
-  });
-//   switch (e.message.content.toUpperCase()) {
-//     case "$BTC":
-//       priceGetter('btc', (price) => {
-//         e.message.channel.sendMessage(`Live **Bitcoin** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//     case "$ETH":
-//       priceGetter('eth', (price) => {
-//         e.message.channel.sendMessage(`Live **Ethereum** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//     case "$LTC":
-//       priceGetter('ltc', (price) => {
-//         e.message.channel.sendMessage(`Live **LiteCoin** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//     case "$XRP":
-//       priceGetter('xrp', (price) => {
-//         e.message.channel.sendMessage(`Live **Ripple** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//     case "$BCC":
-//       priceGetter('bcc', (price) => {
-//         e.message.channel.sendMessage(`Live **Bitcoin Cash** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//     case "$DOGE":
-//       priceGetter('doge', (price) => {
-//         e.message.channel.sendMessage(`Live **DogeCoin** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//     case "$NEO":
-//       priceGetter('neo', (price) => {
-//         e.message.channel.sendMessage(`Live **Neo** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//     case "$ZEC":
-//       priceGetter('zec', (price) => {
-//         e.message.channel.sendMessage(`Live **Zcash** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//     case "$DASH":
-//       priceGetter('dash', (price) => {
-//         e.message.channel.sendMessage(`Live **DashCoin** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//     case "$ETC":
-//       priceGetter('etc', (price) => {
-//         e.message.channel.sendMessage(`Live **Ethereum Classic** price: ${"```javascript"}
-// $${price} ${"```"}`);
-//       });
-//       break;
-//   }
+      e.message.channel.sendMessage(discordMessage);
+      return;
+    })''
+  }
 });
